541b77a5e919e743035b3b0389fc167cb11c79aa
Harshal Chamorshikar <72064675+Harshal210902@users.noreply.github.com>
2023-08-03 20:55:33 +0530

    Create SegmentTree.cpp

---
+++ SegmentTree.cpp
@@ -0,0 +1,44 @@
+class SGTree{
+    vector<int> seg;
+public:
+    SGTree(int n)
+    {
+        seg.resize(4 * n + 1);
+    }
+    void build(int ind, int low, int high, int arr[])
+    {
+        if(low==high)
+        {
+            seg[ind] = arr[low];
+            return;
+        }
+        int mid = low + (high-low)/2;
+        build(2*ind+1, low, mid, arr);
+        build(2*ind+2,mid+1,high,arr);
+        seg[ind] = min(seg[2*ind+1],seg[2*ind+2]);//var
+    }
+
+    int query(int ind, int l, int r, int low, int high)
+    {
+        if(l>high || r<low) return INT_MAX;
+        if(l<=low && high<=r) return seg[ind];
+        
+        int mid = low+ (high-low)/2;
+        int left= query(2*ind+1, l, r, low, mid);
+        int right = query(2*ind+2,l,r,mid+1,high);
+        return min(left,right);//var
+    }
+
+    void update(int ind, int low, int high, int i, int val)
+    {
+        if(low==high)
+        {
+            seg[ind] = val;
+            return;
+        }
+        int mid = low + (high-low)/2;
+        if(i<=mid) update(2*ind+1,low,mid,i,val);
+        else update(2*ind+2,mid+1,high,i,val);
+        seg[ind] = min(seg[2*ind+1],seg[2*ind+2]);//var
+    }
+};
