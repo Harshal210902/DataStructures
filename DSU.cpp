1aaf4a864c1cf6689198d4600e4fa4988a095d75
Harshal Chamorshikar <72064675+Harshal210902@users.noreply.github.com>
2023-08-03 20:58:27 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,43 @@
+class Disjointset{
+    vector<int> rank, parent, size;
+public:
+    Disjointset(int n)
+    {
+        rank.resize(n+1,0);
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i =0;i<=n;i++) parent[i] = i;
+    }
+    int findUPar(int node)
+    {
+        if(node == parent[node]) return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_u]>rank[ulp_v]) parent[ulp_v] = ulp_u;
+        else if(rank[ulp_u]<rank[ulp_v]) parent[ulp_u] = ulp_v;
+        else {
+            parent[ulp_u] = ulp_v;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        } 
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+};
